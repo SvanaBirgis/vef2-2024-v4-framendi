acaf1bf98f0cbaf8af5a38c38d00117e65a4807a
Svana Björg <100528784+SvanaBirgis@users.noreply.github.com>
2024-03-21 23:26:27 +0000

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -24,8 +24,8 @@ export default async function Home() {
       <h1 className={styles.title}>Welcome to the Football League 2023-2024!</h1>
         <p className={styles.aboutContainer}> Step into the heart of the game with our interactive site, 
         your ultimate portal for the Football League 2023-2024! Here, every match played is at your 
-        fingertips, with the power to dive deep into the action. But that's not all – you're in control! 
-        Add or delete matches to tailor your experience, ensuring you're always in sync with the pulse of 
+        fingertips, with the power to dive deep into the action. But that&apos;s not all – you&apos;re in control! 
+        Add or delete matches to tailor your experience, ensuring you&apos;re always in sync with the pulse of 
         the league. Embrace the excitement of the season and shape your journey through the thrilling world 
         of football! </p>
         {games?.length > 0 && <p className={styles.titleGameGrid}>Here you can see the 5 latest games that have been played.</p>}
